35891c548f67228f7ebd32b292a65007fde74414
killer0428penjp-star <killer0428penjp@gmail.com>
2026-02-12 05:39:49 +0000

    update

---
+++ js/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+  getFirestore, collection, getDocs, doc, setDoc, deleteDoc, updateDoc, 
+  query, orderBy, onSnapshot, serverTimestamp, limit, addDoc, getDoc, arrayUnion 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = { 
+    apiKey: "AIzaSyBkKXKmrV-vzq3OoUrvihd4X9KGKpjoNBc", 
+    authDomain: "calender-98a9e.firebaseapp.com", 
+    projectId: "calender-98a9e" 
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// 必要な関数をまとめてエクスポート
+export { 
+  db, collection, getDocs, doc, setDoc, deleteDoc, updateDoc, 
+  query, orderBy, onSnapshot, serverTimestamp, limit, addDoc, getDoc, arrayUnion 
+};
