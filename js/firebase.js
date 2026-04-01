2526d38d66972ad1e96924ce156e02a2ef09a524
killer0428penjp-star <killer0428penjp@gmail.com>
2026-04-01 05:47:40 +0000

    aaa

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,21 @@
-import { getFirestore, collection, getDocs, doc, setDoc, deleteDoc, updateDoc, 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+  getFirestore, collection, getDocs, doc, setDoc, deleteDoc, updateDoc, 
   query, orderBy, onSnapshot, serverTimestamp, limit, addDoc, getDoc, 
-  arrayUnion, arrayRemove  // ← 追加
+  arrayUnion, arrayRemove
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-const firebaseConfig = { apiKey: "AIzaSyBkKXKmrV-vzq3OoUrvihd4X9KGKpjoNBc", authDomain: "calender-98a9e.firebaseapp.com", projectId: "calender-98a9e" };
+
+const firebaseConfig = { 
+  apiKey: "AIzaSyBkKXKmrV-vzq3OoUrvihd4X9KGKpjoNBc", 
+  authDomain: "calender-98a9e.firebaseapp.com", 
+  projectId: "calender-98a9e" 
+};
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, getDocs, doc, setDoc, deleteDoc, updateDoc, query, 
-  orderBy, onSnapshot, serverTimestamp, limit, addDoc, getDoc, 
-  arrayUnion, arrayRemove  // ← 追加
+export { 
+  db, collection, getDocs, doc, setDoc, deleteDoc, updateDoc, 
+  query, orderBy, onSnapshot, serverTimestamp, limit, addDoc, getDoc, 
+  arrayUnion, arrayRemove
 };
